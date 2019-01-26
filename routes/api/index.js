6b979b824beead41e668819ef394a2680632b94e
Bryan Magers <bmagers@users.noreply.github.com>
2019-01-26 11:55:20 -0800

    items routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const router = require("express").Router();
 const todoRoutes = require("./todo");
-
+const itemRoutes = require("./items");
 
 // todo routes
 router.use("/todo", todoRoutes);
+router.use("/items", itemRoutes);
 
 // For anything else, render the html page
 router.use(function(req, res) {
--- routes/api/items.js
+++ routes/api/items.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db/models");
+const db = require("../../db/models");
 
 router.get("/test", function(req, res) {
   res.json("Hello world!");
