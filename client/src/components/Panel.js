dbcc002c5da4a0d98c7c8d3f53e3903fa491b876
Bryan Magers <bmagers@users.noreply.github.com>
2019-02-03 13:24:28 -0800

    props updates

--- client/src/components/Panel.js
+++ client/src/components/Panel.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Link from "./Link";
 
-// props.links is an array of objects, each containing a linkTitle and a linkUrl
-// iterate through props.links using array.map() and return a link component for each index
-
 const Panel = props => {
-  let { panelType, panelTitle, links } = props;
+  const { browser, selectedPage, thisColumn, id } = props;
+  const { panelType, panelTitle, links } = browser.pages[selectedPage].columns[thisColumn].panels[id];
+
   if (panelType === "linkPanel") {
     return (
       <div className="panel">
         <div className="panelTitle">{panelTitle}</div>
         <div>{ links.map((obj, index) =>
-          <Link linkUrl={obj.linkUrl} linkTitle={obj.linkTitle} key={index} />
+          <Link browser={browser} selectedPage={selectedPage} thisColumn={thisColumn} linkUrl={obj.linkUrl} linkTitle={obj.linkTitle} key={index} />
         )}</div>
       </div>
     );
--- client/src/components/SaveButton.js
+++ client/src/components/SaveButton.js
@@ -112,12 +112,130 @@ function saveData() {
   BrowserApi.putBrowser(browserObject);
 }
 
+function testApi() {
+  const browserObject = {
+    browser: {
+      pages: [
+        {
+          pageTitle: "Here's page one of my super awesome links.",
+          columns: [
+            {
+              panels: [
+                {
+                  panelType: "linkPanel",
+                  panelTitle: "web tools",
+                  links: [
+                    {
+                      linkTitle: "Google",
+                      linkUrl: "https://www.google.com"
+                    },
+                    {
+                      linkTitle: "Wikipedia",
+                      linkUrl: "https://www.wikipedia.com"
+                    }
+                  ]
+                },
+                {
+                  panelType: "linkPanel",
+                  panelTitle: "social media",
+                  links: [
+                    {
+                      linkTitle: "Facebook",
+                      linkUrl: "https://www.facebook.com"
+                    },
+                    {
+                      linkTitle: "Instagram",
+                      linkUrl: "https://www.instagram.com"
+                    },
+                    {
+                      linkTitle: "LinkedIn",
+                      linkUrl: "https://www.linkedin.com"
+                    },
+                    {
+                      linkTitle: "Reddit",
+                      linkUrl: "https://www.reddit.com"
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              panels: [
+                {
+                  panelType: "linkPanel",
+                  panelTitle: "news sources",
+                  links: [
+                    {
+                      linkTitle: "The New York Times",
+                      linkUrl: "https://www.nytimes.com"
+                    },
+                    {
+                      linkTitle: "Washington Post",
+                      linkUrl: "https://www.washingtonpost.com"
+                    },
+                    {
+                      linkTitle: "National Public Radio",
+                      linkUrl: "https://www.npr.org"
+                    },
+                    {
+                      linkTitle: "CNN",
+                      linkUrl: "https://www.cnn.com"
+                    },
+                    {
+                      linkTitle: "Seattle Times",
+                      linkUrl: "https://www.seattletimes.com"
+                    },
+                    {
+                      linkTitle: "Politico",
+                      linkUrl: "https://www.politico.com"
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              panels: [
+                {
+                  panelType: "linkPanel",
+                  panelTitle: "fun stuff",
+                  links: [
+                    {
+                      linkTitle: "Internet Movie Database",
+                      linkUrl: "https://www.imdb.com"
+                    },
+                    {
+                      linkTitle: "All Music Guide",
+                      linkUrl: "https://www.allmusic.com"
+                    },
+                    {
+                      linkTitle: "Airbnb",
+                      linkUrl: "https://www.airbnb.com"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  };
+  BrowserApi.addPanel(browserObject, 0, 0, "linkPanel", "Test Panel");
+}
+
 const SaveButton = props => {
   return (
-    <div className="SaveButton">
-      <button onClick={function() { saveData() }}>Save data to /api/browser</button>
+    <div>
+      <div className="SaveButton">
+        <button onClick={ function() { saveData() } }>Save data to /api/browser</button>
+      </div>
+
+      <div className="TestButton">
+        <button onClick={ function() { testApi() } }>Test API</button>
+      </div>
     </div>
-  )
+    
+  );
 }
 
 export default SaveButton;
