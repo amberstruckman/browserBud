24ed0f0f5b463771c360389cd13eb9d3d4497d7e
Bryan Magers <bmagers@users.noreply.github.com>
2019-02-06 18:29:09 -0800

    delete links part 1

--- client/src/components/Link.js
+++ client/src/components/Link.js
@@ -3,8 +3,12 @@ import React from "react";
 // return a single link
 
 const Link = props => {
+
+  const { browser, selectedPage, editMode, id, update } = props;
+
   return (
     <div className="Link">
+      {editMode && <div className="minusDiv"><span className="minus">-</span></div>}
       <a href={props.linkUrl}>{props.linkTitle}</a>
     </div>
   )
